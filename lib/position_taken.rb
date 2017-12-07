3ecb95a98ccffa55e1acde46e7b33ff6467327f4
Emma Freeman <freemoe04@gmail.com>
2017-12-07 22:59:18 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,3 @@
 def position_taken?(board, position)
-   !(board[position].nil? || board[position] != " ")
+   !(board[position].nil? || board[position] == " ")
 end# code your #position_taken? method here!
