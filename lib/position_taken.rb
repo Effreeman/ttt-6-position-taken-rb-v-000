6843aae5fa97d66604eb04b238440cdcf23439f3
Emma Freeman <freemoe04@gmail.com>
2017-12-08 00:07:33 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
-+def position_taken?(board, index)
- +  board[index] == "X" && board[index] =="O" || board[index] == "X" || board[index] =="O"
- +end
+def position_taken?(board, index)
+    board[index] == "X" && board[index] =="O" || board[index] == "X" || board[index] =="O"
+end
 
  # code your #position_taken? method here!
